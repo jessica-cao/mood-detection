24d08859fb9dcbb7df703964b17148caec1676b6
jessica-cao <jessicacao1756@gmail.com>
2019-09-15 11:59:56 -0400

    putting the flask app and cropper together

--- crop_webcam_image.py
+++ crop_webcam_image.py
@@ -1,12 +1,15 @@
 import cv2 as cv
 import numpy as np 
+import flask_app.app as app
 
 # using this to make sure that the image actually displays
 import matplotlib.pyplot as plt
 
 # load open cv's cascade, open the image,change it to grayscale, and crop the image out
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-image = cv.imread('s005-03_img.bmp')
+# camera = app.success()
+# image = cv.imread(camera)
+image = cv.imread('flask_app/image.bmp')
 gray_img = cv.cvtColor(image, cv.IMREAD_GRAYSCALE)
 
 faces = face_cascade.detectMultiScale(gray_img, 1.1, 4)
