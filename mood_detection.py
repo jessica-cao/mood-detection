59f006f31fdb448e4912524ec18929cb7a6e1b7d
Oliver Zhang <oliverzh2000@gmail.com>
2019-09-14 23:38:48 -0400

    Fix image array formatting

--- mood_detection.py
+++ mood_detection.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 import tensorflow as tf
-from tensorflow.python.keras import layers
+from tensorflow.python.keras import layers, utils
 from skimage.color import rgb2lab
 import numpy as np
 from PIL import Image
@@ -19,17 +19,24 @@ for person in os.listdir(train_dir):
     for img_dir in os.listdir(train_dir / Path(person) / Path('bmp')):
         if count < 7:
             new_img = Image.open(train_dir / Path(person) / Path('bmp') / Path(img_dir), 'r')
+            print(train_dir / Path(person) / Path('bmp') / Path(img_dir))
             resized_img = new_img.resize((64, 64), Image.ANTIALIAS)
+            resized_img = np.array(resized_img)
             train.append(resized_img)
             label.append(count)
             count += 1
 
-train = np.array(train, dtype=float)
+train = np.array(train)
+print(train)
+label = np.array(label)
 train = 1.0/255*train
+
+num_classes = 7
+label = utils.to_categorical(label, num_classes)
 
 # the ML model
 model = tf.keras.Sequential()
-model.add(layers.InputLayer(input_shape=(28, 28, 3)))
+model.add(layers.InputLayer(input_shape=(64, 64, 3)))
 model.add(layers.Conv2D(16, (2, 2), padding='same', activation='relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(layers.Conv2D(32, (2, 2),  padding='same', activation='relu'))
@@ -57,12 +64,16 @@ for people in os.listdir(test_directory):
             if count < 7:
                 new_img = Image.open(test_directory + '/' + file, 'r')
                 resized_img = new_img.resize((64, 64), Image.ANTIALIAS)
+                resized_img = np.array(resized_img)
                 test.append(resized_img)
                 test_label.append(count)
                 count += 1
 
 test = np.array(test, dtype=float)
+test_label = np.array(test_label)
 test = 1.0/255*test
+
+test_label = utils.to_categorical(test_label, num_classes)
 
 score = model.evaluate(test, test_label, verbose=1)
 print('\n', 'Test accuracy:', score[1])
